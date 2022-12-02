bb72c69bfbf6dcf1a2acef0b12e5dbf8697082b2
Miguel <miguee02@gmail.com>
2022-12-02 18:04:05 +0100

    added order feature

--- src/pages/Hotels/index.tsx
+++ src/pages/Hotels/index.tsx
@@ -1,14 +1,25 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
 import { Hotel } from '../../api/types';
 import GenericList from '../../components/GenericList';
 import HotelItem from '../../components/HotelItem';
-import { Text } from '../../components/Themed';
+import { Text, View } from '../../components/Themed';
 import useHotels from '../../hooks/useHotels';
 import { HotelsProps } from '../types';
+import styles from './styles';
 
 const List: FC<HotelsProps> = (props) => {
     const { navigation } = props;
-    const { hotels, search, loading } = useHotels();
+    const { hotels, search, order, loading } = useHotels();
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState<keyof Hotel | ''>('');
+    const [items, setItems] = useState([
+        { label: 'Default', value: '' },
+        { label: 'Name', value: 'name' },
+        { label: 'Stars', value: 'stars' },
+        { label: 'User rating', value: 'userRating' },
+        { label: 'Price', value: 'price' },
+    ]);
 
     useEffect(() => {
         navigation.setOptions({
@@ -20,17 +31,37 @@ const List: FC<HotelsProps> = (props) => {
         });
     }, [navigation]);
 
+    useEffect(() => {
+        order(value);
+    }, [value]);
+
     if (loading) {
         return <Text>Loading...</Text>;
     }
 
     return (
-        <GenericList
-            items={hotels}
-            renderDetails={HotelItem}
-            onPressItem={(item: Hotel) => console.log({ item })}
-            keyExtractor={(item: Hotel) => item.id}
-        />
+        <>
+            <View style={styles.header}>
+                <Text>Order by</Text>
+                <View style={styles.picker}>
+                    <DropDownPicker
+                        placeholder="Default"
+                        open={open}
+                        value={value}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setValue}
+                        setItems={setItems}
+                    />
+                </View>
+            </View>
+            <GenericList
+                items={hotels}
+                renderDetails={HotelItem}
+                onPressItem={(item: Hotel) => console.log({ item })}
+                keyExtractor={(item: Hotel) => item.id}
+            />
+        </>
     );
 };
 
